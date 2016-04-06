ec99336cf620cce120d2defe2db0c13acd565a03
root <root@iZ25hv4hw7lZ.(none)>
2016-04-06 11:46:57 +0800

    add test script

--- pcache.php
+++ pcache.php
@@ -1,27 +1,11 @@
 <?php
-$re=pcache_add('abc','abc');
+$re=pcache_add('abc','abc',20);
 var_dump($re);
 $re=pcache_get('abc');
 var_dump($re);
+$re=pcache_info();
+echo $re."\r\n";
+$re=pcache_del('abc');
+echo pcache_info()."\r\n";
 exit;
-
-$br = (php_sapi_name() == "cli")? "":"<br>";
-
-if(!extension_loaded('pcache')) {
-	dl('pcache.' . PHP_SHLIB_SUFFIX);
-}
-$module = 'pcache';
-$functions = get_extension_funcs($module);
-echo "Functions available in the test extension:$br\n";
-foreach($functions as $func) {
-    echo $func."$br\n";
-}
-echo "$br\n";
-$function = 'confirm_' . $module . '_compiled';
-if (extension_loaded($module)) {
-	$str = $function($module);
-} else {
-	$str = "Module $module is not compiled into PHP";
-}
-echo "$str\n";
 ?>
